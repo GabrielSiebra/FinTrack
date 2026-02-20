1146d4e0de7323ea1569fd8f5576f947cd967dc0
Gabriel Siebra <gabrielsiebra2014@gmail.com>
2026-02-20 14:51:44 -0300

    Primeira parte da Main

--- main.java
+++ main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("-- Seja muito bem-vindo--");
+        int escolha = 0;
+        
+        
+        do{
+                System.out.println("Escolha uma opção: ");
+                System.out.println("1 - Consultar saldo");
+                System.out.println("2 - Depositar");
+                System.out.println("3 - Sacar");
+                System.out.println("4 - Sair");
+                escolha = sc.nextInt();
+
+                switch (escolha) {
+                case 1:
+                    
+                    break;
+            
+                case 2:
+                    
+                    break;
+
+                case 3:
+                    
+                    break;
+                    
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+            
+                default:
+                    break;
+            }
+        }while (escolha != 4);
+            sc.close();
+        }
+
     }
-}
+
